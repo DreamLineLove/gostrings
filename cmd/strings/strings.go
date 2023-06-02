e764306f5cde32e04437ae61ccdd248685ab8129
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-02 23:19:26 +0630

    added further showcase in strings.go

--- cmd/strings/strings.go
+++ cmd/strings/strings.go
@@ -13,11 +13,11 @@ func stringsPkg() {
 	WriteSpace(1)
 
 	// Return an all caps version of a string
-	var str1_upper = strings.ToUpper(str1)
+	str1_upper := strings.ToUpper(str1)
 	fmt.Println(str1_upper)
 
 	// Return an all lower caps version of a string
-	var str1_lower = strings.ToLower(str1)
+	str1_lower := strings.ToLower(str1)
 	fmt.Println(str1_lower)
 
 	WriteSpace(2)
@@ -27,10 +27,30 @@ func stringsPkg() {
 	WriteSpace(1)
 
 	// Returns true or false whether a string contains a substring
-	var contains_Normandy = strings.Contains(str2, "Normandy")
+	contains_Normandy := strings.Contains(str2, "Normandy")
 	fmt.Println("Contains \"Normandy\":", contains_Normandy)
 
 	// Returns how many times a string contains a substring
-	var count_the = strings.Count(str2, "the")
+	count_the := strings.Count(str2, "the")
 	fmt.Println("Contains \"the\":", count_the, "times")
+
+	WriteSpace(2)
+
+	var str3 string = "https://github.com/DreamLineLove/gostrings"
+	fmt.Println(str3)
+
+	WriteSpace(1)
+
+	// Returns true or false whether a strings starts with a string
+	starts_with_https := strings.HasPrefix(str3, "https")
+	fmt.Println("Starts with \"https\":", starts_with_https)
+
+	// Returns true or false whether a string ends with a string
+	ends_with_gostrings := strings.HasSuffix(str3, "gostrings")
+	fmt.Println("Ends with \"gostrings\":", ends_with_gostrings)
+
+	// Returns length of string
+	str3_length := len(str3)
+	fmt.Println("Length:", str3_length)
+
 }
