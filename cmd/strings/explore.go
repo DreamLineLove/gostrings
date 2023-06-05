76ab4051448d33592b62684232f2bfd8efc46141
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-05 11:42:01 +0630

    Explored Cut, CutPrefix, and CutSuffix

--- cmd/strings/explore.go
+++ cmd/strings/explore.go
@@ -21,4 +21,43 @@ func explore() {
 	sub2 := "like"
 	count2 := strings.Count(s2, sub2)
 	fmt.Println(sub2, "appears", count2, "times in", s2)
+
+	s2before, s2after, s2result := strings.Cut(s2, ".")
+	s2beforeE, s2afterE, s2resultE := strings.Cut(s2, "...")
+	fmt.Println(s2before + ".")
+	fmt.Println(strings.Trim(s2after, " "))
+	fmt.Println(s2result)
+	fmt.Println(s2beforeE)
+	fmt.Println(s2afterE)
+	fmt.Println(s2resultE)
+	WriteSpace(1)
+
+	google := "https://google.com"
+	protocol, path, googleResult := strings.Cut(google, "://")
+	fmt.Println(protocol)
+	fmt.Println(path)
+	fmt.Println(googleResult)
+	WriteSpace(1)
+
+	justDomain, err := cutPrefixAndSuffixAndPrint(google, "https://", ".com")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	} else {
+		fmt.Println("Just the domain:", justDomain)
+	}
+
+}
+
+func cutPrefixAndSuffixAndPrint(operand string, prefix string, suffix string) (string, error) {
+	noPrefix, found := strings.CutPrefix(operand, prefix)
+	if found {
+		noSuffix, found := strings.CutSuffix(noPrefix, suffix)
+		if found {
+			return noSuffix, nil
+		} else {
+			return "", fmt.Errorf("Suffix not found!")
+		}
+	} else {
+		return "", fmt.Errorf("Prefix not found!")
+	}
 }
